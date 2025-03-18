6187171f35b03701e3b7cd0aceab1bf4f435bcd4
adams <adamskiptalam0@gmail.com>
2025-03-18 22:11:15 +0300

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import { Root, loader as rootLoader } from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      },
+    ],
+  },
+  
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
